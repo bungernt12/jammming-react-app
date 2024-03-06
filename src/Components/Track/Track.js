9e9299f23535bd4c0f2af6543364c303d32300d1
Tague Zachary <taguezz@gmail.com>
2024-03-06 11:18:06 -0800

    changed play button to react-icons, playback now working for single embedded song

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,9 +1,49 @@
 import React from "react";
 import styles from "./Track.module.css";
-// import { useState } from "react";
-import playButton from "../../resources/play-button.svg";
+import { useRef, useState, useEffect } from "react";
+import sound from "./resources/AMD.mp3";
+import {
+  IoPlayBackSharp,
+  IoPlayForwardSharp,
+  IoPlaySkipBackSharp,
+  IoPlaySkipForwardSharp,
+  IoPlaySharp,
+  IoPauseSharp,
+} from "react-icons/io5";
 
 function Track(props) {
+  // const startAudio = () => {
+  //   audioAllMyDays.play();
+  // };
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const [soundObj] = useState(new Audio(sound));
+
+  const togglePlayPause = () => {
+    setIsPlaying((prev) => !prev);
+  };
+
+  useEffect(() => {
+    console.log(isPlaying);
+    isPlaying ? soundObj.play() : soundObj.pause();
+  }, [isPlaying, soundObj]);
+
+  useEffect(() => {
+    return () => {
+      soundObj.pause();
+    };
+  }, [soundObj]);
+
+  // const audioRef = useRef();
+
+  // useEffect(() => {
+  //   if (isPlaying) {
+  //     audioRef.current.play();
+  //   } else {
+  //     audioRef.current.pause();
+  //   }
+  // }, [isPlaying, audioRef]);
+
   return (
     // This is not working right now. I don't see a border.
     <div className={styles.trackBox}>
@@ -18,7 +58,11 @@ function Track(props) {
       in the results list section and - button if its rendered in the playlist section.
       Also trying to re-enable button of the same track when - button is clicked. */}
       <div className={styles.trackButtons}>
-        <img src={playButton} alt="play-button" className={styles.playButton} />
+        {/* <img src={playButton} alt="play-button" className={styles.playButton} /> */}
+        <button onClick={togglePlayPause}>
+          {isPlaying ? <IoPauseSharp /> : <IoPlaySharp />}
+        </button>
+        {/* <audio src={sound} ref={audioRef} /> */}
         {props.trackInPlaylist ? (
           <button
             className={styles.button}
